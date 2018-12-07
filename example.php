36586f003024098ad261078237583c7fd887c5d4
Jordan Hall <jordan@hall05.co.uk>
2018-12-07 22:30:15 +0000

    Improve example

--- example.php
+++ example.php
@@ -2,9 +2,28 @@
 
 use Carbon\Carbon;
 use DivineOmega\GitHubStatusApi\Client;
+use DivineOmega\GitHubStatusApi\Enums\GitHubStatus;
 
 require_once 'vendor/autoload.php';
 
 $status = (new Client())->status(Carbon::now());
 
-var_dump($status);
+switch ($status) {
+    case GitHubStatus::GOOD:
+        echo 'GitHub is up! No issues reported.';
+        break;
+
+    case GitHubStatus::MINOR:
+        echo 'GitHub is experiencing minor issues.';
+        break;
+
+    case GitHubStatus::MAJOR:
+        echo 'GitHub is experiencing major issues.';
+        break;
+
+    case GitHubStatus::UNKNOWN:
+        echo 'Unable to determine GitHub\'s status.';
+        break;
+}
+
+echo PHP_EOL;
